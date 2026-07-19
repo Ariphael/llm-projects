d975642b4f0bac4d77d1b01b297a123d5bd492ad
Richard Tochtermann <richard.tochtermann@outlook.com>
2026-07-19 17:55:01 +1000

    NEW: added LLMQueryRetryLimitExceeded exception

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,3 +1,7 @@
 class EmptyToolCallBlockException(Exception):
   """Exception raised when <tool_calls></tool_calls> does not contain any tool calls"""
   pass
+
+class LLMQueryRetryLimitExceeded(Exception):
+  """Exception raised when LLM query POST request fails over QUERY_LLM_MAX_RETRIES times"""
+  pass
