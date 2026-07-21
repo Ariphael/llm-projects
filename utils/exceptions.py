99f2b7bad3503c3ec4bedaa3f708634a81c86e32
Richard Tochtermann <richard.tochtermann@outlook.com>
2026-07-21 17:27:30 +1000

    NEW: created new exception LLMQueryClientError which to be raised when an LLM query POST request returns a status code within the 4xx range, but not 429 Timeout

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -5,3 +5,7 @@ class EmptyToolCallBlockException(Exception):
 class LLMQueryRetryLimitExceeded(Exception):
   """Exception raised when LLM query POST request fails over QUERY_LLM_MAX_RETRIES times"""
   pass
+
+class LLMQueryClientError(Exception):
+  """Exception raised when an LLM query POST request returns status code 4xx. Not raised when status code is 429 (Timeout)"""
+  pass
